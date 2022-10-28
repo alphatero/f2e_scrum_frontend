19a8b9fab3f12e5f4bed2ed2e7d6f17935658ec0
Alpha <46735230+alphatero@users.noreply.github.com>
2022-10-28 22:20:25 +0800

    Feature/change node version (#5)
    * feat:install plugin - framer-motion to set router transition
    
    * feat:install clsx to manage class css
    
    * feat: 🎸 fixed node version in package json
    
    Co-authored-by: elzuoc <elzuoc@gmail.com>
    Co-authored-by: alpha <102722070+lucentgit@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,62 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  useLocation,
+  Outlet,
 } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import NavBar from './components/NavBar';
 import ExampleLearn from './views/ExampleLearn';
 import ExampleChallenge from './views/ExampleChallenge';
 import Home from './views/Home';
 import './App.css';
 
+const pageVariants = {
+  initial: {
+    opacity: 0,
+  },
+  in: {
+    opacity: 1,
+  },
+  out: {
+    opacity: 0,
+  },
+};
+
+const pageTransition = {
+  type: 'tween',
+  ease: 'linear',
+  duration: 0.5,
+};
+
+function AnimationLayout() {
+  const { pathname } = useLocation();
+  return (
+    <motion.div
+      key={pathname}
+      initial="initial"
+      animate="in"
+      variants={pageVariants}
+      transition={pageTransition}
+    >
+      <Outlet />
+    </motion.div>
+  );
+}
+
 function App() {
   return (
 
-    <div className="App">
-
-      <hr />
-      <Router forceRefresh>
+    <div>
+      <Router>
         <NavBar />
         <hr />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ExampleLearn" element={<ExampleLearn />} />
-          <Route path="/ExampleChallenge" element={<ExampleChallenge />} />
+          <Route element={<AnimationLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/ExampleLearn" element={<ExampleLearn />} />
+            <Route path="/ExampleChallenge" element={<ExampleChallenge />} />
+          </Route>
         </Routes>
       </Router>
     </div>
