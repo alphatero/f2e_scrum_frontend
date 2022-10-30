54b7a772e2528970c251dd0f32985f35090ed372
zoeGuava <zoeguava@gmail.com>
2022-10-31 00:31:43 +0800

    feat: install prop-types and set previousPage prop

--- src/components/introduceScrum/NavBar.jsx
+++ src/components/introduceScrum/NavBar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { LeftArrow } from '../icon';
 
-function NavBar() {
+function NavBar({ previousPage }) {
   return (
     <div className="flex flex-row items-center gap-3 px-5 py-3">
       <div className="w-2">
         <LeftArrow />
       </div>
-      <p className="inline-block">Scrum 新手村專案</p>
+      <p className="inline-block">{ previousPage }</p>
     </div>
   );
 }
+
+NavBar.propTypes = {
+  previousPage: PropTypes.string,
+};
+
+NavBar.defaultProps = {
+  previousPage: 'previous page\'s name',
+};
 
 export default NavBar;
--- src/views/IntroduceScrum.jsx
+++ src/views/IntroduceScrum.jsx
@@ -3,12 +3,13 @@ import { NavBar, Avatar } from '../components/introduceScrum';
 
 function IntroduceScrum() {
   return (
-    <>
-      <NavBar />
+    <div>
+      <NavBar previousPage="Scrum 新手村專案" />
+      <hr />
       <h3>IntroduceScrum is here</h3>
       <p>page content</p>
       <Avatar />
-    </>
+    </div>
   );
 }
 
