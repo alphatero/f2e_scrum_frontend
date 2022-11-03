7cad9c733f65166d0bbd9ab269d378447188cfa7
alpha <alphatero@icloud.com>
2022-11-03 11:04:35 +0800

    feat: 🎸 disabled react props-types rule
    add yarn.lock in .gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:react/jsx-runtime',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:react/jsx-runtime'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
     'max-len': ['warn', { code: 80 }],
+    'react/prop-types': 'off',
   },
 };
