e7d96d43bc0d37e65a858467bd42c5a6dfcfaa5f
Alpha <46735230+alphatero@users.noreply.github.com>
2022-11-03 16:03:50 +0800

    Feature/introduction role (#9)
    * feat:install plugin - framer-motion to set router transition
    
    * feat:install clsx to manage class css
    
    * feat: 🎸 add introduction role page
    
    add components, pages, add eslint roles
    
    * refactor: 💡 with new eslint rule
    
    Co-authored-by: elzuoc <elzuoc@gmail.com>

---
+++ src/constants/navbarList.js
@@ -0,0 +1,24 @@
+export const PageList = [
+  {
+    id: 0,
+    link: '/',
+    label: 'Home',
+  },
+  {
+    id: 1,
+    link: '/ExampleLearn',
+    label: 'Learn Example',
+  },
+  {
+    id: 2,
+    link: '/ExampleChallenge',
+    label: 'Challenge Example',
+  },
+  {
+    id: 3,
+    link: '/IntroductionRole',
+    label: 'Introduction Role',
+  },
+];
+
+export default PageList;
