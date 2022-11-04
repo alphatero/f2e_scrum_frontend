11065639109026c7539517152e991e4b5a8094a6
elzuoc <elzuoc@gmail.com>
2022-11-04 12:31:26 +0800

    feat: clsx usage, use component

---
+++ src/constants/backlogSettingInfo.js
@@ -0,0 +1,40 @@
+export const BacklogSettingInfo = {
+  title: 'Sprint Backlog 實作',
+  tasks: [
+    {
+      id: 0,
+      contant:
+        '會員系統(登入、註冊、權限管理)',
+      priority: '優先度高',
+      point: 8,
+      seq: 0,
+    },
+    {
+      id: 1,
+      contant:
+        '前台職缺列表(職缺詳細內容、點選可發送應徵意願)',
+      priority: '優先度中',
+      point: 8,
+      seq: 1,
+    },
+    {
+      id: 2,
+      contant:
+        '後台職缺管理(資訊上架、下架、顯示應徵者資料)',
+      priority: '優先度低',
+      point: 5,
+      seq: 2,
+    },
+    {
+      id: 3,
+      contant:
+        '應徵者的線上履歷編輯器',
+      priority: '優先度高',
+      point: 13,
+      seq: 3,
+    },
+  ],
+  button: '開始 Sprint ！',
+};
+
+export default BacklogSettingInfo;
--- src/views/BacklogSetting.jsx
+++ src/views/BacklogSetting.jsx
@@ -1,49 +1,47 @@
 import clsx from 'clsx';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../components';
+import { BacklogSettingInfo } from '../constants/backlogSettingInfo';
 
 function BacklogSetting() {
+  const navigate = useNavigate();
+  const { tasks, button } = BacklogSettingInfo;
+
+  const backHome = () => {
+    navigate('/');
+  };
   return (
-    <div className={clsx({ 'p-4': true })}>
+    <div className="p-4">
 
-      <div className={clsx({ 'border border-zinc-800 rounded-lg p-3': true })}>
-        {/* first task */}
-        <div className={clsx({ 'w-full grid grid-cols-4 gap-2 rounded-lg bg-zinc-200 p-3': true })}>
-          <div className={clsx({ 'col-span-3': true })}>會員系統(登入、註冊、權限管理)</div>
-          <div>
-            <div className={clsx({ 'rounded-lg p-1 bg-white text-center': true })}>優先度高</div>
-          </div>
-        </div>
-
-        {/* second task */}
-        <div className={clsx({ 'w-full grid grid-cols-4 gap-2 rounded-lg bg-zinc-200 p-3 mt-3': true })}>
-          <div className={clsx({ 'col-span-3': true })}>前台職缺列表(職缺詳細內容、點選可發送應徵意願)</div>
-          <div>
-            <div className={clsx({ 'rounded-lg p-1 bg-white text-center': true })}>優先度中</div>
-          </div>
-        </div>
-
-        {/* second task */}
-        <div className={clsx({ 'w-full grid grid-cols-4 gap-2 rounded-lg bg-zinc-200 p-3 mt-3': true })}>
-          <div className={clsx({ 'col-span-3': true })}>後台職缺管理(資訊上架、下架、顯示應徵者資料)</div>
-          <div>
-            <div className={clsx({ 'rounded-lg p-1 bg-white text-center': true })}>優先度低</div>
-          </div>
-        </div>
-
-        {/* second task */}
-        <div className={clsx({ 'w-full grid grid-cols-4 gap-2 rounded-lg bg-zinc-200 p-3 mt-3': true })}>
-          <div className={clsx({ 'col-span-3': true })}>應徵者的線上履歷編輯器</div>
-          <div>
-            <div className={clsx({ 'rounded-lg p-1 bg-white text-center': true })}>優先度高</div>
-          </div>
-        </div>
+      <div className="border border-zinc-800 rounded-lg px-3 pb-3">
+        {
+          tasks.map((task) => (
+            <div className={clsx(
+              'w-full',
+              'grid grid-cols-4 gap-2',
+              'rounded-lg bg-zinc-200',
+              'p-3 mt-3',
+            )}
+            >
+              <div className="col-span-3">{task.contant}</div>
+              <div>
+                <div className="rounded-lg p-1 bg-white text-center">{task.priority}</div>
+              </div>
+            </div>
+          ))
+        }
 
       </div>
-      <div className={clsx({ 'w-full h-5 text-center m-3': true })}>﹀ ﹀ ﹀</div>
-      <div className={clsx({ 'w-full h-5 text-right mb-1': true })}>優先度高</div>
-      <div className={clsx({ 'border border-zinc-800 rounded-lg p-3 h-[40vh]': true })}>
+      <div className="w-full h-5 text-center m-3">﹀ ﹀ ﹀</div>
+      <div className="w-full h-5 text-right mb-1">優先度高</div>
+      <div className="border border-zinc-800 rounded-lg p-3 h-[40vh]">
         {/* drag to here */}
       </div>
-      <div className={clsx({ 'w-full h-5 text-right mb-1': true })}>優先度低</div>
+      <div className="w-full h-5 text-right mb-1">優先度低</div>
+
+      <div className="flex justify-center">
+        <Button onClick={backHome}>{button}</Button>
+      </div>
 
     </div>
   );
