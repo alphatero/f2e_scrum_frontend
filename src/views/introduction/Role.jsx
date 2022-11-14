31e6cbb9ef787801faa8e85fb7ec2a4e84aafb5d
elzuoc <elzuoc@gmail.com>
2022-11-14 12:30:46 +0800

    refactor: use component to handle the same UI

--- src/views/introduction/Role.jsx
+++ src/views/introduction/Role.jsx
@@ -3,6 +3,77 @@ import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components';
 import { RoleInfo } from '../../constants/roleInfo';
 
+function FrameUI({ ChildUI }) {
+  return (
+    <div className={clsx(
+      'grid grid-flow-row auto-rows-max',
+      'border border-zinc-800 rounded-lg',
+      'p-3 mb-5',
+    )}
+    >
+      {ChildUI}
+
+    </div>
+  );
+}
+
+function ChildOpenUI({ card }) {
+  return (
+    <div className="w-full">
+      <div className="w-full mb-5 flex">
+        <div className="flex-1">
+          <h3 className="text-xl">{card.title}</h3>
+          <h6 className="mb-8">{card.subtitle}</h6>
+          <h6>{card.introTitle}</h6>
+          <p>{card.introBriefly}</p>
+        </div>
+        <div className="flex-none">
+          <div className="relative w-40 h-40">
+            <div className={clsx(
+              'absolute m-auto inset-0',
+              'border border-zinc-800',
+              'w-32 h-40',
+            )}
+            />
+            <img
+              className={clsx('absolute m-auto inset-0 top-[-40px]', 'w-40 h-52')}
+              alt={`${card.title}`}
+              src={`${card.img}`}
+            />
+          </div>
+        </div>
+
+      </div>
+
+      <div className="w-full">
+        {card.intro.map((part) => (
+          <p className="mb-5">
+            {part}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ChildCloseUI({ card }) {
+  return (
+    <div className="w-full flex">
+
+      <div className="flex-1">
+        <h3 className="text-xl">{card.title}</h3>
+        <h6>{card.subtitle}</h6>
+      </div>
+      <div className="flex-none">
+        <div className="relative w-24">
+          <img className="absolute m-auto inset-0 top-[20px] h-20" alt={`${card.title}`} src={`${card.img}`} />
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
 function Role() {
   const navigate = useNavigate();
   const { cards, button } = RoleInfo;
@@ -18,73 +89,16 @@ function Role() {
       {
         cards.map((card, idx) => (isOpen[idx] ? (
 
-          <div className={clsx(
-            'grid grid-flow-row auto-rows-max',
-            'border border-zinc-800 rounded-lg',
-            'p-3 mb-5',
-          )}
-          >
-            <div className="w-full mb-5 flex">
-              <div className="flex-1">
-                <h3 className="text-xl">{card.title}</h3>
-                <h6 className="mb-8">{card.subtitle}</h6>
-                <h6>{card.introTitle}</h6>
-                <p>{card.introBriefly}</p>
-              </div>
-              <div className="flex-none">
-                <div className="relative w-40 h-40">
-                  <div className={clsx(
-                    'absolute m-auto inset-0',
-                    'border border-zinc-800',
-                    'w-32 h-40',
-                  )}
-                  />
-                  <img
-                    className={clsx('absolute m-auto inset-0 top-[-40px]', 'w-40 h-52')}
-                    alt={`${card.title}`}
-                    src={`${card.img}`}
-                  />
-                </div>
-              </div>
-
-            </div>
-
-            <div className="w-full">
-              {card.intro.map((part) => (
-                <p className="mb-5">
-                  {part}
-                </p>
-              ))}
-            </div>
-          </div>
+          <FrameUI ChildUI={<ChildOpenUI card={card} />} />
 
         ) : (
 
-          <div className={clsx(
-            'grid grid-flow-row auto-rows-max',
-            'border border-zinc-800 rounded-lg',
-            'p-3 mb-5',
-          )}
-          >
-            <div className="w-full flex">
-
-              <div className="flex-1">
-                <h3 className="text-xl">{card.title}</h3>
-                <h6>{card.subtitle}</h6>
-              </div>
-              <div className="flex-none">
-                <div className="relative w-24">
-                  <img className="absolute m-auto inset-0 top-[20px] h-20" alt={`${card.title}`} src={`${card.img}`} />
-                </div>
-
-              </div>
-            </div>
-          </div>
+          <FrameUI ChildUI={<ChildCloseUI card={card} />} />
 
         )))
       }
 
-      <div className="absolute bottom-10 w-[90vw] flex justify-center">
+      <div className="relative w-full flex justify-center">
         <Button onClick={backHome}>{button}</Button>
       </div>
     </div>
