2bf9da7aaf2506deeeb9af6533bbcf3524d96750
elzuoc <elzuoc@gmail.com>
2022-11-18 10:04:21 +0800

    feat: new props in component

--- src/components/BlurBlockBg.jsx
+++ src/components/BlurBlockBg.jsx
@@ -1,13 +1,18 @@
 import clsx from 'clsx';
 
 export function BlurBlockBg(props) {
-  const { children } = props;
+  const {
+    children, backdropBlur = 'backdrop-blur-md', rounded = 'rounded-md', padding = 'p-3',
+  } = props;
   return (
 
     <div className={clsx(
-      'w-full border border-gray-100 rounded-md',
-      'bg-gray-400 bg-clip-padding bg-opacity-10',
-      'backdrop-filter backdrop-blur-md',
+      'w-full border border-white',
+      'bg-gradient-to-b from-white/70 to-white/30 bg-clip-padding bg-opacity-70',
+      'backdrop-filter',
+      padding,
+      rounded,
+      backdropBlur,
     )}
     >
       {children}
