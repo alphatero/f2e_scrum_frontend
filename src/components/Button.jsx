b67ac717212fe644d128201e52b173bb800a502e
elzuoc <elzuoc@gmail.com>
2022-12-28 15:53:47 +0800

    fix: max width setting, move static variable outside component function

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,30 +1,33 @@
 import clsx from 'clsx';
 
+const btn = {
+  primary: `
+  text-white bg-teal-500 
+  hover:bg-teal-400 
+  active:bg-teal-600 
+  disabled:bg-slate-300
+  `,
+  secondary: `
+  bg-white
+  border-2 border-teal-500 text-teal-500 
+  hover:border-teal-400 hover:bg-teal-50 hover:text-teal-400 
+  active:border-teal-600 active:text-teal-600 
+  disabled:border-slate-300 disabled:text-slate-300
+  `,
+};
+
 export function Button(
   {
     children, onClick, className,
-    btnType = 'primary', disabled = false,
+    maxWidth = '358px',
+    btnType = 'primary',
+    disabled = false,
   },
 ) {
-  const btn = {
-    primary: `
-    text-white bg-teal-500 
-    hover:bg-teal-400 
-    active:bg-teal-600 
-    disabled:bg-slate-300
-    `,
-    secondary: `
-    bg-white
-    border-2 border-teal-500 text-teal-500 
-    hover:border-teal-400 hover:bg-teal-50 hover:text-teal-400 
-    active:border-teal-600 active:text-teal-600 
-    disabled:border-slate-300 disabled:text-slate-300
-    `,
-  };
-
   return (
     <div className={clsx(
-      'relative flex-col w-full max-w-[358px]',
+      'relative flex-col w-full',
+      maxWidth === 'none' ? 'max-w-none' : `max-w-[${maxWidth}]`,
       className,
     )}
     >
