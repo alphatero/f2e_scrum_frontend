290046d6af14772100db4cf3e6dda56101175239
zoeGuava <zoeguava@gmail.com>
2023-01-05 01:04:07 +0800

    update: 牌卡開合的時候給圖片的縮放加上動畫

--- src/components/introduction/Role/ItemHeaderImg.jsx
+++ src/components/introduction/Role/ItemHeaderImg.jsx
@@ -4,7 +4,7 @@ export function ItemHeaderImg({ card, openState }) {
   return (
     <div className="flex-none">
       <div className={clsx(
-        'relative',
+        'relative transition-all duration-500 ease-linear',
         openState ? 'w-40 h-40' : 'w-24 md:w-28',
       )}
       >
@@ -18,7 +18,6 @@ export function ItemHeaderImg({ card, openState }) {
         <img
           className={clsx(
             'absolute m-auto inset-x-0 bottom-0',
-            'transition duration-1000 ease-linear',
             'md:w-full md:h-auto',
             openState ? 'w-40' : 'top-4 h-20',
           )}
