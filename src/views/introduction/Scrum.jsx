eb1cc87e8d623f3961a229fbfca368f2c92b30ab
elzuoc <elzuoc@gmail.com>
2023-02-04 11:45:58 +0800

    feat: intruduction/scrum - use axios to call api to show info.

--- src/views/introduction/Scrum.jsx
+++ src/views/introduction/Scrum.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   IntroductionLayout,
 } from '../../components/introduction/IntroductionLayout';
-import { scrumInfo } from '../../constants/introductionScrum';
+// import { scrumInfo } from '../../constants/introductionScrum';
+
+const apiGetScrumInfo = async () => axios.get('https://f2e-scrum-backend.onrender.com/api/introduction/scrum');
 
 export function Scrum() {
-  return (
-    <IntroductionLayout info={scrumInfo} />
-  );
+  const [scrumInfo, setScrumInfo] = useState({});
+
+  useEffect(() => {
+    const getScrumInfo = async () => {
+      try {
+        const { data } = await apiGetScrumInfo();
+        // console.log(data);
+        setScrumInfo(data);
+      } catch (error) {
+        console.log(error.message); //  eslint-disable-line no-console
+      }
+    };
+    getScrumInfo();
+  }, []);
+
+  if (Object.keys(scrumInfo).length) {
+    return (
+      <IntroductionLayout info={scrumInfo} />
+    );
+  }
 }
 
 export default Scrum;
