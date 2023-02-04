5f18d4b283fb3defa11576b0d23dae0a2acbbdaf
elzuoc <elzuoc@gmail.com>
2023-02-04 16:28:19 +0800

    fix: remove unnecessary remark, return to home page when data is wrong or not found

--- src/views/introduction/Scrum.jsx
+++ src/views/introduction/Scrum.jsx
@@ -1,33 +1,49 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import {
   IntroductionLayout,
 } from '../../components/introduction/IntroductionLayout';
-// import { scrumInfo } from '../../constants/introductionScrum';
 
 const apiGetScrumInfo = async () => axios.get('https://f2e-scrum-backend.onrender.com/api/introduction/scrum');
 
 export function Scrum() {
+  const navigate = useNavigate();
   const [scrumInfo, setScrumInfo] = useState({});
+  const neccessaryProperty = ['guide', 'titles', 'tag', 'image', 'article', 'button', 'next'];
+  let isReturn = true;
+
+  const backHome = () => {
+    navigate('/');
+  };
 
   useEffect(() => {
     const getScrumInfo = async () => {
       try {
         const { data } = await apiGetScrumInfo();
-        // console.log(data);
         setScrumInfo(data);
       } catch (error) {
         console.log(error.message); //  eslint-disable-line no-console
+        backHome();
       }
     };
     getScrumInfo();
   }, []);
 
   if (Object.keys(scrumInfo).length) {
-    return (
-      <IntroductionLayout info={scrumInfo} />
-    );
+    neccessaryProperty.forEach((item) => {
+      if (!Object.prototype.hasOwnProperty.call(scrumInfo, item)) {
+        isReturn = false;
+      }
+    });
+
+    if (isReturn) {
+      return (
+        <IntroductionLayout info={scrumInfo} />
+      );
+    }
   }
+  return backHome();
 }
 
 export default Scrum;
