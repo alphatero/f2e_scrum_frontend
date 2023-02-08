9b2039cc6884b6343265a222899a06e6b44b2716
zoeGuava <zoeguava@gmail.com>
2023-02-09 01:08:50 +0800

    refactor: 💡 透過 axios 來取得 introduction/role 的文案

--- src/views/introduction/Role.jsx
+++ src/views/introduction/Role.jsx
@@ -1,14 +1,16 @@
 import clsx from 'clsx';
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components';
 import { ItemContainer, ItemChild } from '../../components/introduction/Role';
-import { RoleInfo } from '../../constants/introductionRoleInfo';
+// import { RoleInfo } from '../../constants/introductionRoleInfo';
 
 export function Role() {
   const navigate = useNavigate();
   const [isOpenObj, setIsOpenObj] = useState({ card_0: true, card_1: false, card_2: false });
-  const { cards, button } = RoleInfo;
+  // const { cards, button } = RoleInfo;
+  const [pageContent, setPageContent] = useState({});
   const nextPage = () => {
     navigate('/introduction/sprint-guide');
   };
@@ -16,10 +18,26 @@ export function Role() {
     navigate(-1);
   };
 
+  async function fetchContent(url) {
+    try {
+      const { data } = await axios.get(url);
+      console.log('data: ', data);
+      setPageContent(() => data);
+      console.log('pageContentData: ', pageContent);
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  }
+
+  useEffect(() => {
+    const URL = 'https://f2e-scrum-backend.onrender.com/api/introduction/role';
+    fetchContent(URL);
+  }, []);
+
   return (
     <div className="flex flex-col h-full p-4 pt-8">
       {
-        cards.map((card, idx) => (
+        pageContent.cards && pageContent.cards.map((card, idx) => (
           <ItemContainer
             key={`card_${card.id}`}
             openState={isOpenObj[`card_${idx}`]}
@@ -46,7 +64,7 @@ export function Role() {
         'flex justify-center items-center flex-col gap-5',
       )}
       >
-        <Button onClick={nextPage}>{button}</Button>
+        <Button onClick={nextPage}>{pageContent.button}</Button>
         <Button onClick={prevPage} btnType="secondary">回上頁</Button>
       </div>
     </div>
