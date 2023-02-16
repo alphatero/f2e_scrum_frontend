6c6f7d499b51786dffddf8a9e0a162f62e592e29
zoeGuava <zoeguava@gmail.com>
2023-02-16 22:42:15 +0800

    feat: 🎸 移除 headers 的設定，目前都是純文案不太需要

--- src/api/index.js
+++ src/api/index.js
@@ -4,12 +4,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export function getRequest(path, params) {
   const requestUrl = BASE_URL + path;
-  const headers = new Headers({
-    Accept: 'application/json',
-    'Content-type': 'application/json',
-  });
   const config = {
-    headers,
     ...params,
   };
   return axios.get(requestUrl, config);
