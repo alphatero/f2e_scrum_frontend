edd2690adbdbeaba14db223c5fd217e4aed8ad93
elzuoc <elzuoc@gmail.com>
2023-02-18 16:36:13 +0800

    fix: introductionLayout-image property may not exist, feat: create .env.local

--- src/views/introduction/Scrum.jsx
+++ src/views/introduction/Scrum.jsx
@@ -1,28 +1,15 @@
 import { IntroductionLayout } from '../../components';
 import { Api } from '../../components/Api';
-import { BlurBlockBg } from '../../components/BlurBlockBg';
 
 const apiSet = {
   requirProp: ['guide', 'titles', 'tag', 'image', 'article', 'button', 'next'],
-  apiUrl: 'https://f2e-scrum-backend.onrender.com/api/introduction/scrum',
+  apiUrl: process.env.REACT_APP_API_INTRODUCTION_SCRUM,
 };
-
-const loadingPage = () => (
-  <div className="h-screen flex place-content-center">
-    <div className="self-center w-fit h-fit p-10 max-w-5xl mx-auto">
-      <BlurBlockBg type="B">
-        <span className="text-5xl text-slate-400 mr-3">Loading</span>
-        <span className="text-xl text-slate-400">page...</span>
-        <p className="text-2xl text-slate-500 mt-3">Please wait a moment.</p>
-      </BlurBlockBg>
-    </div>
-  </div>
-);
 
 export function Scrum() {
   const result = Api(apiSet);
 
-  if (!result) return loadingPage();
+  if (!result) return (<p>loading</p>);
 
   return (
     <IntroductionLayout info={result} />
--- src/views/introduction/SprintPoint.jsx
+++ src/views/introduction/SprintPoint.jsx
@@ -1,28 +1,15 @@
 import { IntroductionLayout } from '../../components';
 import { Api } from '../../components/Api';
-import { BlurBlockBg } from '../../components/BlurBlockBg';
 
 const apiSet = {
   requirProp: ['guide', 'titles', 'tag', 'article', 'button', 'next'],
-  apiUrl: 'https://f2e-scrum-backend.onrender.com/api/introduction/sprint-point',
+  apiUrl: process.env.REACT_APP_API_INTRODUCTION_SPRINTPOINT,
 };
-
-const loadingPage = () => (
-  <div className="h-screen flex place-content-center">
-    <div className="self-center w-fit h-fit p-10 max-w-5xl mx-auto">
-      <BlurBlockBg type="B">
-        <span className="text-5xl text-slate-400 mr-3">Loading</span>
-        <span className="text-xl text-slate-400">page...</span>
-        <p className="text-2xl text-slate-500 mt-3">Please wait a moment.</p>
-      </BlurBlockBg>
-    </div>
-  </div>
-);
 
 export function IntroSprintPoint() {
   const result = Api(apiSet);
 
-  if (!result) return loadingPage();
+  if (!result) return (<p>loading</p>);
 
   return (
     <IntroductionLayout info={result} />
