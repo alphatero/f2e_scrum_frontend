03774783943dc3c401edb5ebb2a4bb544bd74c34
zoeGuava <zoeguava@gmail.com>
2023-02-22 01:09:00 +0800

    feat: 🎸 如果資料尚未回傳，或是回傳的資料不符合預期，便只顯示 loading...

--- src/views/introduction/Role.jsx
+++ src/views/introduction/Role.jsx
@@ -7,6 +7,7 @@ import { fetchContent, verifyKeyIsMatch } from '../../api';
 // import { RoleInfo } from '../../constants/introductionRoleInfo';
 
 export function Role() {
+  const [fetchState, setFetchState] = useState(false);
   const navigate = useNavigate();
   const [isOpenObj, setIsOpenObj] = useState({ card_0: true, card_1: false, card_2: false });
   const [pageContent, setPageContent] = useState({});
@@ -24,46 +25,54 @@ export function Role() {
         const verifyData = resultData.cards[0];
 
         if (verifyKeyIsMatch(responseCardKeys, verifyData)) {
+          setFetchState(true);
           setPageContent(resultData);
+        } else {
+          setFetchState(false);
         }
       });
   }, []);
 
-  return (
-    <div className="flex flex-col h-full p-4 pt-8">
-      {
-        pageContent.cards && pageContent.cards.map((card, idx) => (
-          <ItemContainer
-            key={`card_${card.id}`}
-            openState={isOpenObj[`card_${idx}`]}
-            onClick={() => {
-              setIsOpenObj({
-                card_0: false,
-                card_1: false,
-                card_2: false,
-                [`card_${idx}`]: true,
-              });
-            }}
-          >
-            <ItemChild
-              key={`child_${card.id}`}
-              card={card}
+  if (fetchState) {
+    return (
+      <div className="flex flex-col h-full p-4 pt-8">
+        {
+          pageContent.cards && pageContent.cards.map((card, idx) => (
+            <ItemContainer
+              key={`card_${card.id}`}
               openState={isOpenObj[`card_${idx}`]}
-            />
-          </ItemContainer>
-        ))
-      }
+              onClick={() => {
+                setIsOpenObj({
+                  card_0: false,
+                  card_1: false,
+                  card_2: false,
+                  [`card_${idx}`]: true,
+                });
+              }}
+            >
+              <ItemChild
+                key={`child_${card.id}`}
+                card={card}
+                openState={isOpenObj[`card_${idx}`]}
+              />
+            </ItemContainer>
+          ))
+        }
 
-      <div className={clsx(
-        'mx-auto mt-auto md:mb-11 w-full',
-        'flex justify-center items-center flex-col gap-5',
-      )}
-      >
-        <Button onClick={nextPage}>{pageContent.button}</Button>
-        <Button onClick={prevPage} btnType="secondary">回上頁</Button>
+        <div className={clsx(
+          'mx-auto mt-auto md:mb-11 w-full',
+          'flex justify-center items-center flex-col gap-5',
+        )}
+        >
+          <Button onClick={nextPage}>{pageContent.button}</Button>
+          <Button onClick={prevPage} btnType="secondary">回上頁</Button>
+        </div>
       </div>
-    </div>
 
+    );
+  }
+  return (
+    <h2>loading...</h2>
   );
 }
 
