37317374fae1390091375977b248c65457c2d4b0
zoeGuava <zoeguava@gmail.com>
2023-02-23 01:09:10 +0800

    feat: 🎸 將 introduction/sprint-guide 的內容改為由 api 取得

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,6 +1,7 @@
 export const apiResponseVerifyKeys = {
   introduction: {
     role: ['id', 'img', 'intro', 'introBriefly', 'introTitle', 'priority', 'subtitle', 'title'],
+    sprintGuide: ['article', 'button', 'guide', 'image', 'next', 'tag', 'titles'],
   },
 };
 
--- src/views/introduction/SprintGuide.jsx
+++ src/views/introduction/SprintGuide.jsx
@@ -1,11 +1,33 @@
-import {
-  IntroductionLayout,
-} from '../../components/introduction/IntroductionLayout';
-import { sprintGuideInfo } from '../../constants/introductionSprintGuide';
+import { useState, useEffect } from 'react';
+import { IntroductionLayout } from '../../components/introduction/IntroductionLayout';
+import { fetchContent, verifyKeyIsMatch } from '../../api';
+import { apiResponseVerifyKeys } from '../../api/constants';
 
 export function SprintGuide() {
+  const [fetchState, setFetchState] = useState(false);
+  const [pageContent, setPageContent] = useState({});
+
+  useEffect(() => {
+    fetchContent('/introduction/sprint-guide')
+      .then((resultData) => {
+        const verifyData = Object.keys(resultData);
+
+        if (verifyKeyIsMatch(apiResponseVerifyKeys.introduction.sprintGuide, verifyData)) {
+          setFetchState(true);
+          setPageContent(resultData);
+        } else {
+          setFetchState(false);
+        }
+      });
+  }, []);
+
+  if (!fetchState) {
+    return (
+      <h2>loading...</h2>
+    );
+  }
   return (
-    <IntroductionLayout info={sprintGuideInfo} />
+    <IntroductionLayout info={pageContent} />
   );
 }
 
