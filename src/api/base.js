d5391b39b4ab0563216bf6191f1cdd12afeda3c4
Alpha <46735230+alphatero@users.noreply.github.com>
2023-03-02 11:54:27 +0800

    feat: 🎸 add base api function (#88)

---
+++ src/api/base.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const baseURL = process.env.REACT_APP_API_URL;
+
+async function api(url, method, params) {
+  try {
+    const { data } = await axios[method](baseURL + url, params);
+
+    return data;
+  } catch (error) {
+    throw new Error('message:', error);
+  }
+}
+
+function get(url) {
+  return api(url, 'get');
+}
+
+export const Api = {
+  get,
+};
+
+export default Api;
