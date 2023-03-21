b04cbc125adc21942be5b2a593eea1b5f08120bd
elzuoc <elzuoc@gmail.com>
2023-03-21 10:29:31 +0800

    feat: exam's page

--- src/views/exam/ExamProductBacklog.jsx
+++ src/views/exam/ExamProductBacklog.jsx
@@ -1,14 +1,26 @@
+import { useState, useEffect } from 'react';
 import { DragLayout } from 'components';
-import { ExamProductBacklogInfo } from 'constants/examProductBacklogInfo';
+// import { ExamProductBacklogInfo } from 'constants/examProductBacklogInfo';
+import { Api } from '../../api/index';
+
+const apiUrl = '/exam/product-backlog';
 
 export function ExamProductBacklog() {
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    Api.get(apiUrl).then((data) => setInfo(data));
+  }, []);
+
+  if (!Object.keys(info).length) return (<p>loading</p>);
+
   const speechTexts = {
     ready: '拖曳任務試試看吧！',
     error: '唉呀！優先度好像擺放錯誤囉！再試試看！',
   };
 
   return (
-    <DragLayout info={ExamProductBacklogInfo} speechTexts={speechTexts} />
+    <DragLayout info={info} speechTexts={speechTexts} />
   );
 }
 
