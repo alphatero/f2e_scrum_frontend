c244390987472b8c8777b85d84498342c14b5298
elzuoc <elzuoc@gmail.com>
2023-04-26 09:39:40 +0800

    feat: use typescript in introduction/ProductBacklog

---
+++ src/views/introduction/ProductBacklog.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import { IntroductionLayout } from 'components';
+import { Api } from 'api';
+
+const apiUrl = '/introduction/product-backlog';
+
+
+interface ArticleType {
+  content: string[],
+  id: string,
+  type: string
+};
+interface ImageType {
+  type: string,
+  src: string,
+  reference: string
+};
+interface TitleType {
+  content: string,
+  id: string
+};
+interface InfoType {
+  article?: ArticleType[],
+  button?: string,
+  guide?: string,
+  image?: ImageType,
+  next?: string,
+  tag?: string,
+  titles?: TitleType[]
+};
+
+export function IntroProductBacklog() {
+  const [info, setInfo] = useState<InfoType>({});
+
+  useEffect(() => {
+    Api.get(apiUrl).then((data) => setInfo(data as InfoType));
+  }, []);
+
+  if (!Object.keys(info).length) return <p>loading</p>;
+
+  return (
+    <IntroductionLayout info={info} />
+  );
+}
+
+export default IntroProductBacklog;
